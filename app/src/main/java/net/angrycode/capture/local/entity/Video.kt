b27587c3e96deee5418e1ebc91a62a02b0b520ad
wecodexyz <wecodexyz@gmail.com>
2018-01-04 18:19:40 +0800

    video play

--- app/src/main/java/net/angrycode/capture/local/entity/Video.kt
+++ app/src/main/java/net/angrycode/capture/local/entity/Video.kt
@@ -3,4 +3,4 @@ package net.angrycode.capture.local.entity
 /**
  * Created by pc on 2018/1/3.
  */
-data class Video(var path: String, var name: String, var thumb: String, var createTime: Long = 0, val duration: Long = 0)
+data class Video(var name: String, var path: String, var thumb: String, var createTime: Long = 0, val duration: Long = 0)
--- app/src/main/java/net/angrycode/capture/local/ui/CaptureListFragment.kt
+++ app/src/main/java/net/angrycode/capture/local/ui/CaptureListFragment.kt
@@ -1,12 +1,17 @@
 package net.angrycode.capture.local.ui
 
 import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import net.angrycode.capture.MainActivity
 import net.angrycode.capture.R
 import net.angrycode.capture.base.BaseAdapter
 import net.angrycode.capture.base.BaseViewHolder
 import net.angrycode.capture.base.CommonListFragment
+import net.angrycode.capture.ext.toast
 import net.angrycode.capture.listLocalVideos
 import net.angrycode.capture.local.adapter.CaptureListAdapter
 import net.angrycode.capture.local.entity.Video
@@ -14,6 +19,7 @@ import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import pub.devrel.easypermissions.AfterPermissionGranted
 import pub.devrel.easypermissions.EasyPermissions
+import java.io.File
 
 /**
  * Capture video file list in local storage.
@@ -46,6 +52,19 @@ class CaptureListFragment : CommonListFragment<Video, BaseViewHolder>() {
             uiThread {
                 adapter.setNewData(localList)
             }
+        }
+    }
+
+    override fun doOnItemClick(view: View, position: Int) {
+        val video = adapter.getItem(position)
+        val intent = Intent(Intent.ACTION_VIEW)
+        val uri = Uri.parse("file://${video?.path}")
+        intent.setDataAndType(uri, "video/*")
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        if (context?.packageManager?.resolveActivity(intent, 0) != null) {
+            startActivity(intent)
+        } else {
+            toast("Cannot not found a video player.")
         }
     }
 
